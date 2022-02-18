98774f1be5a15e3b934ee8e6672439bdd500fc97
raucha <raucha@users.noreply.github.com>
2022-02-18 11:04:22 +0900

    Update initdb.sh

--- docker/initdb.sh
+++ docker/initdb.sh
@@ -3,6 +3,9 @@ set -e
 
 service postgresql start
 
+# 参考:
+# https://hub.docker.com/_/postgres/
+
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     alter role postgres with password 'XXXXX';
